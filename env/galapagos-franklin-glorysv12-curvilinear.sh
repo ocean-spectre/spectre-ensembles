fa866d1160182d373f791b30a091f98ed038f4bf
Joe Schoonover <joe@fluidnumerics.com>
2026-01-12 19:53:15 -0500

    Clean out rectilinear grid configuraiton for glorys

--- env/galapagos-franklin-glorysv12-curvilinear.sh
+++ env/galapagos-franklin-glorysv12-curvilinear.sh
@@ -1,9 +1,10 @@
 export spectre_cwd=/group/tdgs/joe/spectre-150-ensembles #$(pwd) # The local full path to the spectre-ensembles repository
 export spectre_optfile="${spectre_cwd}/opt/galapagos-franklin" # The opt file you want to use to build with
 export spectre_mitgcm_source_code="${spectre_cwd}/MITgcm" # The path the the MITgcm source code
-export spectre_model_code=$spectre_cwd/simulations/glorysv12/code # The `code/` directory with MITgcm customizations and build time parameters
-export spectre_model_input=$spectre_cwd/simulations/glorysv12/code # Where your model input deck comes from
-export spectre_model_exe_dir=$spectre_cwd/exe # Where you will run the MITgcm
+export spectre_model_code=$spectre_cwd/simulations/glorysv12-curvilinear/code # The `code/` directory with MITgcm customizations and build time parameters
+export spectre_model_input=$spectre_cwd/simulations/glorysv12-curvilinear/code # Where your model input deck comes from
+#export spectre_model_exe_dir=$spectre_cwd/simulations/glorysv12-curvilinear/benchmark # Where you will run the MITgcm
+export spectre_model_exe_dir=/apps/workspace/joe # Where you will run the MITgcm
 
 
 module --force purge
